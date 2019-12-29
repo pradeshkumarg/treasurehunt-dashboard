5996db63575df79b8fe28403f87535d5a44661fd
Pradesh Kumar <pradeshkumar1@outlook.com>
2019-12-29 20:54:44 +0530

    Converting to a node app

---
+++ app.js
@@ -0,0 +1,14 @@
+// init
+const express = require('express');
+var app = module.exports = express();
+const cors = require('cors');
+const path=require('path');
+
+app.use(express.static(__dirname + "/public"));
+app.use(cors());
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log(`Everything works fine !! Server started in http://localhost:${port}`);
+})
